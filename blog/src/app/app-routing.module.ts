4887f276333f471398da901051859f0aa3388752
Ethelynaparecida <ethelynaparecida@gmail.com>
2021-05-06 12:47:30 -0300

    envio crud tema

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { TemaDeletComponent } from './edit/tema-delet/tema-delet.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
@@ -11,7 +13,10 @@ const routes: Routes = [
   {path:'entrar', component:EntrarComponent},
   {path:'cadastrar', component:CadastrarComponent},
   {path:'inicio', component: InicioComponent},
-  {path:'tema', component: TemaComponent}
+  {path:'tema', component: TemaComponent},
+  {path: 'tema-edit/:id', component: TemaEditComponent},
+  {path: 'tema-delet/:id', component: TemaDeletComponent}
+
 
 ];
 
